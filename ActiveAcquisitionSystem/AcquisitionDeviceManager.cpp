087dc6dded248e065266ea53bbad93d4558f1636
cxnvcarol <cxnvcarol@gmail.com>
2017-07-07 15:55:58 +0200

    update

--- ActiveAcquisitionSystem/AcquisitionDeviceManager.cpp
+++ ActiveAcquisitionSystem/AcquisitionDeviceManager.cpp
@@ -1,16 +1,63 @@
 #include "AcquisitionDeviceManager.h"
-#include "VimbaCPP/Include/VimbaCPP.h"
 
-using namespace AVT::VmbAPI;
+
+//using namespace AVT::VmbAPI;
 using namespace std;
 
 AcquisitionDeviceManager::AcquisitionDeviceManager()
 {
-	sistema = &VimbaSystem::GetInstance();
-	err = sistema->Startup();
+	initializeAPIs();
 }
 
 
 AcquisitionDeviceManager::~AcquisitionDeviceManager()
 {
+}
+
+bool AcquisitionDeviceManager::initializeAPIs()
+{
+	//vimba
+	sistema = &AVT::VmbAPI::VimbaSystem::GetInstance();
+	err = sistema->Startup();
+
+	//texas dlp
+
+
+	//canon
+
+
+	return true;
+
+}
+void AcquisitionDeviceManager::endAPIs()
+{
+	//to be called at the end of the session
+	//return true;
+}
+
+void AcquisitionDeviceManager::detectCameras()
+{
+
+}
+
+void AcquisitionDeviceManager::detectProjectors()
+{
+
+}
+ActiveCamera* AcquisitionDeviceManager::getCameras()
+{
+	throw "nop";
+}
+ActiveCamera* AcquisitionDeviceManager::getCamera(string dev_id)
+{
+	return NULL;
+}
+
+Projector* AcquisitionDeviceManager::getProjectors()
+{
+	throw "nop";
+}
+Projector* AcquisitionDeviceManager::getProjector(string dev_id)
+{
+	return NULL;
 }
--- ActiveAcquisitionSystem/AcquisitionDeviceManager.h
+++ ActiveAcquisitionSystem/AcquisitionDeviceManager.h
@@ -1,5 +1,7 @@
 #pragma once
-
+#include "VimbaCPP/Include/VimbaCPP.h"
+#include "Camera.h"
+#include "Projector.h"
 class AcquisitionDeviceManager
 {
 	/*
@@ -8,10 +10,19 @@ class AcquisitionDeviceManager
 public:
 	AcquisitionDeviceManager();
 	~AcquisitionDeviceManager();
+	bool initializeAPIs();
+	void endAPIs();
+	void detectCameras();
+	void detectProjectors();
+	
+	ActiveCamera* getCameras();
+	ActiveCamera* getCamera(std::string dev_id);
+	Projector* getProjector(std::string dev_id);
+	Projector* getProjectors();
 
 private:
-	VimbaSystem* sistema;
+	AVT::VmbAPI::VimbaSystem* sistema;
 	VmbErrorType    err;
-	
+	ActiveCamera* cameraList;
 };
 
