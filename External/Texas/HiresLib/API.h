0d2acc01843a4981c4a957a18da0cdc833b2e120
cxnvcarol <cxnvcarol@gmail.com>
2017-08-27 17:15:39 +0200

    fix x previous refactor to specify DLP device

--- External/Texas/HiresLib/API.h
+++ External/Texas/HiresLib/API.h
@@ -51,166 +51,166 @@ typedef struct
 
 typedef void API_DataCallback_t(void *Param, API_CommandInfo_t *CmdInfo);
 
-int LCR_SoftwareReset(void);
+int LCR_SoftwareReset(hid_device* devHandle);
 
 /* System status commands */
 int LCR_GetStatus(unsigned char *pHWStatus, unsigned char *pSysStatus,
-                                                    unsigned char *pMainStatus);
+                                                    unsigned char *pMainStatus, hid_device* devHandle);
 int LCR_GetVersion(unsigned int *pApp_ver, unsigned int *pAPI_ver,
-                    unsigned int *pSWConfig_ver, unsigned int *pSeqConfig_ver);
-int LCR_GetFrmwVersion(unsigned int *pFrmwType, char *pFrmwTag);
+                    unsigned int *pSWConfig_ver, unsigned int *pSeqConfig_ver, hid_device* devHandle);
+int LCR_GetFrmwVersion(unsigned int *pFrmwType, char *pFrmwTag, hid_device* devHandle);
 
 //TODO Review: 1. I add 1 param to most of fns declared here. 2. I don't know which fns I actually need. 3. It might make more sense to rewrite this as a class or copy it to the DLPProjector instance!
 /* Flash Programing commands */
-int LCR_GetBLStatus(BootLoaderStaus *pBL_Status);
-int LCR_SetFlashType(unsigned char Type);
-int LCR_EnterProgrammingMode(void);
-int LCR_ExitProgrammingMode(void);
-int LCR_GetProgrammingMode(BOOL *ProgMode);
-int LCR_GetFlashPresent(BOOL *pFlashAtCS0, BOOL *pFlashAtCS1, BOOL *pFlashAtCS2);
-int LCR_GetFlashManID(unsigned short *manID);
-int LCR_GetFlashDevID(unsigned long long *devID);
-int LCR_SetFlashAddr(unsigned int Addr);
-int LCR_SetFlashAddr4Byte(unsigned int Addr);
-int LCR_FlashSectorErase(void);
-int LCR_SetDownloadSize(unsigned int dataLen);
-int LCR_SetDownloadSize4Byte(unsigned int dataLen);
-int LCR_DownloadData(unsigned char *pByteArray, unsigned int dataLen);
-void LCR_WaitForFlashReady(void);
-int LCR_CalculateFlashChecksum(void);
-int LCR_GetFlashChecksum(unsigned int*checksum);
-int LCR_EnableMasterSlave(BOOL MasterEnable, BOOL SlaveEnable);
+int LCR_GetBLStatus(BootLoaderStaus *pBL_Status, hid_device* devHandle);
+int LCR_SetFlashType(unsigned char Type, hid_device* devHandle);
+int LCR_EnterProgrammingMode(hid_device* devHandle);
+int LCR_ExitProgrammingMode(hid_device* devHandle);
+int LCR_GetProgrammingMode(BOOL *ProgMode, hid_device* devHandle);
+int LCR_GetFlashPresent(BOOL *pFlashAtCS0, BOOL *pFlashAtCS1, BOOL *pFlashAtCS2, hid_device* devHandle);
+int LCR_GetFlashManID(unsigned short *manID, hid_device* devHandle);
+int LCR_GetFlashDevID(unsigned long long *devID, hid_device* devHandle);
+int LCR_SetFlashAddr(unsigned int Addr, hid_device* devHandle);
+int LCR_SetFlashAddr4Byte(unsigned int Addr, hid_device* devHandle);
+int LCR_FlashSectorErase(hid_device* devHandle);
+int LCR_SetDownloadSize(unsigned int dataLen, hid_device* devHandle);
+int LCR_SetDownloadSize4Byte(unsigned int dataLen, hid_device* devHandle);
+int LCR_DownloadData(unsigned char *pByteArray, unsigned int dataLen, hid_device* devHandle);
+void LCR_WaitForFlashReady(hid_device* devHandle);
+int LCR_CalculateFlashChecksum(hid_device* devHandle);
+int LCR_GetFlashChecksum(unsigned int*checksum, hid_device* devHandle);
+int LCR_EnableMasterSlave(BOOL MasterEnable, BOOL SlaveEnable, hid_device* devHandle);
 
-int LCR_SetPowerMode(unsigned char);
-int LCR_GetPowerMode(BOOL *Standby);
+int LCR_SetPowerMode(unsigned char, hid_device* devHandle);
+int LCR_GetPowerMode(BOOL *Standby, hid_device* devHandle);
 
-int LCR_SetCurtainColor(unsigned int red,unsigned int green, unsigned int blue);
-int LCR_GetCurtainColor(unsigned int *pRed, unsigned int *pGreen, unsigned int *pBlue);
+int LCR_SetCurtainColor(unsigned int red,unsigned int green, unsigned int blue, hid_device* devHandle);
+int LCR_GetCurtainColor(unsigned int *pRed, unsigned int *pGreen, unsigned int *pBlue, hid_device* devHandle);
 
-int LCR_SetDataChannelSwap(unsigned int port, unsigned int swap);
-int LCR_GetDataChannelSwap(unsigned int Port, unsigned int *pSwap);
+int LCR_SetDataChannelSwap(unsigned int port, unsigned int swap, hid_device* devHandle);
+int LCR_GetDataChannelSwap(unsigned int Port, unsigned int *pSwap, hid_device* devHandle);
 
-int LCR_SetPortClock(unsigned int clock);
-int LCR_GetPortClock(unsigned int *pClock);
+int LCR_SetPortClock(unsigned int clock, hid_device* devHandle);
+int LCR_GetPortClock(unsigned int *pClock, hid_device* devHandle);
 
-int LCR_SetInputSource(unsigned int source, unsigned int portWidth);
-int LCR_GetInputSource(unsigned int *pSource, unsigned int *portWidth);
+int LCR_SetInputSource(unsigned int source, unsigned int portWidth, hid_device* devHandle);
+int LCR_GetInputSource(unsigned int *pSource, unsigned int *portWidth, hid_device* devHandle);
 
-int LCR_SetPixelFormat(unsigned int format);
-int LCR_GetPixelFormat(unsigned int *pFormat);
+int LCR_SetPixelFormat(unsigned int format, hid_device* devHandle);
+int LCR_GetPixelFormat(unsigned int *pFormat, hid_device* devHandle);
 
-int LCR_SetTPGSelect(unsigned int pattern);
-int LCR_GetTPGSelect(unsigned int *pPattern);
+int LCR_SetTPGSelect(unsigned int pattern, hid_device* devHandle);
+int LCR_GetTPGSelect(unsigned int *pPattern, hid_device* devHandle);
 
 int LCR_SetTPGColor(unsigned short redFG, unsigned short greenFG,
                     unsigned short blueFG, unsigned short redBG,
-                    unsigned short greenBG, unsigned short blueBG);
+                    unsigned short greenBG, unsigned short blueBG, hid_device* devHandle);
 int LCR_GetTPGColor(unsigned short *pRedFG, unsigned short *pGreenFG,
                     unsigned short *pBlueFG, unsigned short *pRedBG,
-                    unsigned short *pGreenBG, unsigned short *pBlueBG);
+                    unsigned short *pGreenBG, unsigned short *pBlueBG, hid_device* devHandle);
 
-int LCR_LoadSplash(unsigned int index);
-int LCR_GetSplashIndex(unsigned int *pIndex);
+int LCR_LoadSplash(unsigned int index, hid_device* devHandle);
+int LCR_GetSplashIndex(unsigned int *pIndex, hid_device* devHandle);
 
 int LCR_SetLongAxisImageFlip(BOOL);
-BOOL LCR_GetLongAxisImageFlip();
+BOOL LCR_GetLongAxisImageFlip(hid_device* devHandle);
 int LCR_SetShortAxisImageFlip(BOOL);
-BOOL LCR_GetShortAxisImageFlip();
+BOOL LCR_GetShortAxisImageFlip(hid_device* devHandle);
 
-int LCR_SetLedEnables(BOOL SeqCtrl, BOOL Red, BOOL Green, BOOL Blue);
-int LCR_GetLedEnables(BOOL *pSeqCtrl, BOOL *pRed, BOOL *pGreen, BOOL *pBlue);
+int LCR_SetLedEnables(BOOL SeqCtrl, BOOL Red, BOOL Green, BOOL Blue, hid_device* devHandle);
+int LCR_GetLedEnables(BOOL *pSeqCtrl, BOOL *pRed, BOOL *pGreen, BOOL *pBlue, hid_device* devHandle);
 
-int LCR_SetLEDPWMInvert(BOOL invert);
-int LCR_GetLEDPWMInvert(BOOL *inverted);
+int LCR_SetLEDPWMInvert(BOOL invert, hid_device* devHandle);
+int LCR_GetLEDPWMInvert(BOOL *inverted, hid_device* devHandle);
 
 int LCR_GetLedCurrents(unsigned char *pRed, unsigned char *pGreen,
-                                            unsigned char *pBlue);
+                                            unsigned char *pBlue, hid_device* devHandle);
 int LCR_SetLedCurrents(unsigned char RedCurrent, unsigned char GreenCurrent,
-                                            unsigned char BlueCurrent);
+                                            unsigned char BlueCurrent, hid_device* devHandle);
 
 int LCR_SetGPIOConfig(unsigned int pinNum, BOOL dirOutput,
-                                BOOL outTypeOpenDrain, BOOL pinState);
+                                BOOL outTypeOpenDrain, BOOL pinState, hid_device* devHandle);
 int LCR_GetGPIOConfig(unsigned int pinNum, BOOL *pDirOutput,
-                                BOOL *pOutTypeOpenDrain, BOOL *pState);
+                                BOOL *pOutTypeOpenDrain, BOOL *pState, hid_device* devHandle);
 
-int LCR_SetGeneralPurposeClockOutFreq(unsigned int clkId, BOOL enable, unsigned int clkDivider);
-int LCR_GetGeneralPurposeClockOutFreq(unsigned int clkId, BOOL *pEnabled, unsigned int *pClkDivider);
+int LCR_SetGeneralPurposeClockOutFreq(unsigned int clkId, BOOL enable, unsigned int clkDivider, hid_device* devHandle);
+int LCR_GetGeneralPurposeClockOutFreq(unsigned int clkId, BOOL *pEnabled, unsigned int *pClkDivider, hid_device* devHandle);
 
 
-int LCR_SetPWMEnable(unsigned int channel, BOOL Enable);
-int LCR_GetPWMEnable(unsigned int channel, BOOL *pEnable);
+int LCR_SetPWMEnable(unsigned int channel, BOOL Enable, hid_device* devHandle);
+int LCR_GetPWMEnable(unsigned int channel, BOOL *pEnable, hid_device* devHandle);
 int LCR_SetPWMConfig(unsigned int channel, unsigned int pulsePeriod,
-                                            unsigned int dutyCycle);
+                                            unsigned int dutyCycle, hid_device* devHandle);
 int LCR_GetPWMConfig(unsigned int channel, unsigned int *pPulsePeriod,
-                                            unsigned int *pDutyCycle);
+                                            unsigned int *pDutyCycle, hid_device* devHandle);
 
-int LCR_getBatchFileName(unsigned char id, char *batchFileName);
-int LCR_executeBatchFile(unsigned char id);
+int LCR_getBatchFileName(unsigned char id, char *batchFileName, hid_device* devHandle);
+int LCR_executeBatchFile(unsigned char id, hid_device* devHandle);
 
-int LCR_SetMode(int SLmode);
-int LCR_GetMode(int *pMode);
+int LCR_SetMode(int SLmode, hid_device* devHandle);
+int LCR_GetMode(int *pMode, hid_device* devHandle);
 
-int LCR_SetDisplay(rectangle croppedArea, rectangle displayArea);
-int LCR_GetDisplay(rectangle *pCroppedArea, rectangle *pDisplayArea);
+int LCR_SetDisplay(rectangle croppedArea, rectangle displayArea, hid_device* devHandle);
+int LCR_GetDisplay(rectangle *pCroppedArea, rectangle *pDisplayArea, hid_device* devHandle);
 
-int LCR_SetTrigOutConfig(unsigned int trigOutNum, BOOL invert, short rising, short falling);
-int LCR_GetTrigOutConfig(unsigned int trigOutNum, BOOL *pInvert,short *pRising, short *pFalling);
+int LCR_SetTrigOutConfig(unsigned int trigOutNum, BOOL invert, short rising, short falling, hid_device* devHandle);
+int LCR_GetTrigOutConfig(unsigned int trigOutNum, BOOL *pInvert,short *pRising, short *pFalling, hid_device* devHandle);
 
-int LCR_SetTrigIn1Config(BOOL invert, unsigned int trigDelay);
-int LCR_GetTrigIn1Config(BOOL *pInvert, unsigned int *pTrigDelay);
+int LCR_SetTrigIn1Config(BOOL invert, unsigned int trigDelay, hid_device* devHandle);
+int LCR_GetTrigIn1Config(BOOL *pInvert, unsigned int *pTrigDelay, hid_device* devHandle);
 int LCR_SetTrigIn1Delay(unsigned int Delay);
 int LCR_GetTrigIn1Delay(unsigned int *pDelay);
 
-int LCR_SetTrigIn2Config(BOOL invert);
-int LCR_GetTrigIn2Config(BOOL *pInvert);
+int LCR_SetTrigIn2Config(BOOL invert, hid_device* devHandle);
+int LCR_GetTrigIn2Config(BOOL *pInvert, hid_device* devHandle);
 
-int LCR_SetRedLEDStrobeDelay(short rising, short falling);
-int LCR_SetGreenLEDStrobeDelay(short rising, short falling);
-int LCR_SetBlueLEDStrobeDelay(short rising, short falling);
-int LCR_GetRedLEDStrobeDelay(short *, short *);
-int LCR_GetGreenLEDStrobeDelay(short *, short *);
-int LCR_GetBlueLEDStrobeDelay(short *, short *);
+int LCR_SetRedLEDStrobeDelay(short rising, short falling, hid_device* devHandle);
+int LCR_SetGreenLEDStrobeDelay(short rising, short falling, hid_device* devHandle);
+int LCR_SetBlueLEDStrobeDelay(short rising, short falling, hid_device* devHandle);
+int LCR_GetRedLEDStrobeDelay(short *, short *, hid_device* devHandle);
+int LCR_GetGreenLEDStrobeDelay(short *, short *, hid_device* devHandle);
+int LCR_GetBlueLEDStrobeDelay(short *, short *, hid_device* devHandle);
 
-int LCR_PatternDisplay(int Action);
+int LCR_PatternDisplay(int Action, hid_device* devHandle);
 
-int LCR_SetInvertData(BOOL invert);
-int LCR_GetInvertData(BOOL *pInvert);
+int LCR_SetInvertData(BOOL invert, hid_device* devHandle);
+int LCR_GetInvertData(BOOL *pInvert, hid_device* devHandle);
 
-int LCR_SetPatternConfig(unsigned int numLutEntries, unsigned int repeat);
-int LCR_GetPatternConfig(unsigned int *pNumLutEntries, BOOL *pRepeat, unsigned int *pNumPatsForTrigOut2, unsigned int *pNumSplash);
+int LCR_SetPatternConfig(unsigned int numLutEntries, unsigned int repeat, hid_device* devHandle);
+int LCR_GetPatternConfig(unsigned int *pNumLutEntries, BOOL *pRepeat, unsigned int *pNumPatsForTrigOut2, unsigned int *pNumSplash, hid_device* devHandle);
 
 int LCR_ClearPatLut(void);
 int LCR_AddToPatLut(int patNum, int ExpUs, BOOL ClearPat, int BitDepth, int LEDSelect, BOOL WaitForTrigger, int DarkTime, BOOL TrigOut2, int SplashIndex, int BitIndex);
-int LCR_SendPatLut(void);
+int LCR_SendPatLut(hid_device* devHandle);
 
-int LCR_InitPatternMemLoad(BOOL master, unsigned short imageNum, unsigned int size);
-int LCR_pattenMemLoad(BOOL master, unsigned char *pByteArray, int size);
+int LCR_InitPatternMemLoad(BOOL master, unsigned short imageNum, unsigned int size, hid_device* devHandle);
+int LCR_pattenMemLoad(BOOL master, unsigned char *pByteArray, int size, hid_device* devHandle);
 
-int LCR_WriteI2CPassThrough(unsigned int port, unsigned int devadd, unsigned char* wdata, unsigned int nwbytes);
-int LCR_ReadI2CPassThrough(unsigned int port, unsigned int devadd, unsigned char* wdata, unsigned int nwbytes, unsigned int nrbytes, unsigned char* rdata);
-int LCR_I2CConfigure(unsigned int port, unsigned int addm, unsigned int clk);
+int LCR_WriteI2CPassThrough(unsigned int port, unsigned int devadd, unsigned char* wdata, unsigned int nwbytes, hid_device* devHandle);
+int LCR_ReadI2CPassThrough(unsigned int port, unsigned int devadd, unsigned char* wdata, unsigned int nwbytes, unsigned int nrbytes, unsigned char* rdata, hid_device* devHandle);
+int LCR_I2CConfigure(unsigned int port, unsigned int addm, unsigned int clk, hid_device* devHandle);
 
-int LCR_SetPortConfig(unsigned int dataPort,unsigned int pixelClock,unsigned int dataEnable,unsigned int syncSelect);
-int LCR_GetPortConfig(unsigned int *pDataPort,unsigned int *pPixelClock,unsigned int *pDataEnable,unsigned int *pSyncSelect);
+int LCR_SetPortConfig(unsigned int dataPort,unsigned int pixelClock,unsigned int dataEnable,unsigned int syncSelect, hid_device* devHandle);
+int LCR_GetPortConfig(unsigned int *pDataPort,unsigned int *pPixelClock,unsigned int *pDataEnable,unsigned int *pSyncSelect, hid_device* devHandle);
 
-int LCR_enableDebug();
-int LCR_ExecuteRawCommand(uint16 USBCmd, uint08 *Data, int Length);
+int LCR_enableDebug(hid_device* devHandle);
+int LCR_ExecuteRawCommand(uint16 USBCmd, uint08 *Data, int Length, hid_device* devHandle);
 
-int LCR_SetIT6535PowerMode(unsigned int powerMode);
-int LCR_GetIT6535PowerMode(unsigned int *pPowerMode);
+int LCR_SetIT6535PowerMode(unsigned int powerMode, hid_device* devHandle);
+int LCR_GetIT6535PowerMode(unsigned int *pPowerMode, hid_device* devHandle);
 
-int LCR_SetDMDBlocks(int startBlock, int numBlocks);
-int LCR_GetDMDBlocks(int *startBlock, int *numBlocks);
-int LCR_SetDMDSaverMode(short mode);
-int LCR_GetDMDSaverMode();
+int LCR_SetDMDBlocks(int startBlock, int numBlocks, hid_device* devHandle);
+int LCR_GetDMDBlocks(int *startBlock, int *numBlocks, hid_device* devHandle);
+int LCR_SetDMDSaverMode(short mode, hid_device* devHandle);
+int LCR_GetDMDSaverMode(hid_device* devHandle);
 
-int LCR_ReadErrorCode(unsigned int *pCode);
-int LCR_ReadErrorString(char *errStr);
+int LCR_ReadErrorCode(unsigned int *pCode, hid_device* devHandle);
+int LCR_ReadErrorString(char *errStr, hid_device* devHandle);
 
-int LCR_SetMinLEDPulseWidth(int pulseWidthUS);
-int LCR_GetMinLEDPulseWidth(int *pulseWidthUS);
+int LCR_SetMinLEDPulseWidth(int pulseWidthUS, hid_device* devHandle);
+int LCR_GetMinLEDPulseWidth(int *pulseWidthUS, hid_device* devHandle);
 
-int LCR_GetMinPatExposure(int minExposureUS[8]);
+int LCR_GetMinPatExposure(int minExposureUS[8], hid_device* devHandle);
 
 void API_SetDataCallback(API_DataCallback_t *Callback, void *Param);
 int API_GetI2CCommand(char *command, unsigned char *i2cCommand);
